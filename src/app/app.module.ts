dfef2e61064141f318238b428732cbd0ee06cf1f
Abhinay Thakur <a8h1nay@gmail.com>
2020-11-14 20:21:52 +0530

    Create 'Navigation/Sports' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { SportsComponent } from './navigation/sports/sports.component';
+import { SportComponent } from './tile/sport/sport.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    SportsComponent,
+    SportComponent
   ],
   imports: [
     BrowserModule,
